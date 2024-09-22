2024ace91e4b06a6a34b4d8b1f5cf988a8cc94eb
t-shofner <tabithashofner@rocketmail.com>
2024-09-22 18:58:51 -0400

    Initial Commit

--- fruitsales.py
+++ fruitsales.py
@@ -1,1 +1,7 @@
 # add your code here
+import pandas as pd
+
+fruit_sales = pd.DataFrame({'Apples': [35, 21],
+                            'Bananas': [41, 34]},
+                          index=['2017 Sales', '2018 Sales'])
+fruit_sales.to_csv("fruit.csv")
